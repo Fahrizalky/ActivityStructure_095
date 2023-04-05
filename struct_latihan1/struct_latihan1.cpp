14b74bc534358fc96031034c0020d3c2cd008e27
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-04-05 10:35:46 +0700

    Add project files.

---
+++ struct_latihan1/struct_latihan1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140095";
+	mhs1.nama = "fahrizal";
+	mhs1.alamat = "lampung";
+	mhs1.umur = 20;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	cout << "\nNim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
+}
